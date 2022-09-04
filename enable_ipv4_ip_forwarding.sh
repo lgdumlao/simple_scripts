77338a19693441d3116771ebf0fb50443e5a6777
Luis Dumlao <70254493+lgdumlao@users.noreply.github.com>
2022-09-04 11:07:52 +0800

    Update enable_ipv4_ip_forwarding.sh

--- enable_ipv4_ip_forwarding.sh
+++ enable_ipv4_ip_forwarding.sh
@@ -7,3 +7,6 @@ sudo cp /etc/sysctl.conf /etc/sysctl.conf.orig
 
 # enable ip forwarding
 sudo sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1 # updated by ip forward script/' /etc/sysctl.conf
+
+# apply changes
+sudo sysctl -p
